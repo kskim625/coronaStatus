63670f84a21be7e4cbba574f5b7128cb25218485
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-02-01 11:11:14 +0900

    setupProxy test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-module.exports = (app: any) => {
+module.exports = (app) => {
   app.use(createProxyMiddleware('/data/corona'), {
     target: 'https://korea-corona-status-info.herokuapp.com',
     changeOrigin: true,
