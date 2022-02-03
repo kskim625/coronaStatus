014bcd956df0e2321cff88d824571c6249ceb328
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-02-03 10:22:17 +0900

    [update] heroku ssl-redirect 적용
    http로 배포 사이트 접속하면 http로 접속
    -> heroku-ssl-redirect 적용으로 https redirect 시도

--- server/index.ts
+++ server/index.ts
@@ -2,14 +2,16 @@ import express from 'express';
 import dataRouter from './router/dataRouter';
 import bodyParser from 'body-parser';
 import path from 'path';
+import sslRedirect from 'heroku-ssl-redirect';
 
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
 
-app.use('/data', dataRouter);
+app.use(sslRedirect());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../build')));
+app.use('/data', dataRouter);
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
--- server/router/dataRouter.ts
+++ server/router/dataRouter.ts
@@ -3,23 +3,6 @@ import axios from 'axios';
 
 const dataRouter = Router();
 
-export interface objectType {
-  createDt: string;
-  deathCnt: number;
-  defCnt: number;
-  gubun: string;
-  gubunCn: string;
-  gubunEn: string;
-  incDec: number;
-  isolClearCnt: number;
-  localOccCnt: number;
-  overFlowCnt: number;
-  qurRate: string;
-  seq: number;
-  stdDay: string;
-  updateDt: string;
-}
-
 dataRouter.get('/corona', async (req: Request, res: Response, next: NextFunction) => {
   const SERVICE_URL = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson';
   const AUTHORIZATION_KEY = '?serviceKey=Fl9rhYMejA8nhCfRxunEiv8iCWEKK%2FAiNOgmkrp0onGw%2FGIpuTVQc7vH0Kmh%2BaiOeQ6SZSXjk8zaqOdbp9yYTg%3D%3D';
