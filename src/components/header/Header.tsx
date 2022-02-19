9cec85aacfbddf716b7c531ae01bdfa7c4e5fd25
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-02-19 15:22:36 +0900

    [update] Header 컴포넌트 구조 개선
    헤더 컴포넌트 구조 변경(개선)
    - HeaderDateInfo 모듈 생성
        - 위 모듈에서 비즈니스로직 수행
    - Header 모듈은 각 컴포넌트를 불러오기만 하는 식으로

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,59 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react';
-import moment from 'moment';
+import React, { useState } from 'react';
 import flag from '../../images/virus.svg';
-import leftArrow from '../../images/leftArrow.svg';
-import rightArrow from '../../images/rightArrow.svg';
 import HeaderButtons from './HeaderButtons';
+import HeaderDateInfo from './HeaderDateInfo';
 import MessagesModal from '../common/MessagesModal';
 import { objectType } from '../../App';
 import '../../stylesheets/Header.css';
 
+const UpperHeader = () => {
+  return (
+    <div id="header">
+      <img className="header-image" src={flag}></img>
+      <div className="header-description">한국 코로나 상황판</div>
+      <div className="header-dummy"></div>
+    </div>
+  );
+};
+
 const Header = ({ data, getData }: { data: objectType[][]; getData: (query: string) => Promise<void> }) => {
-  const [date, setDate] = useState<moment.Moment>(moment());
   const [modalStatus, setModalStatus] = useState<string>('init');
-  const leftArrowRef = useRef<HTMLImageElement>(null);
-
-  const getNewDate = (newDate: moment.Moment) => {
-    let year = newDate.format('YYYY');
-    let month = newDate.format('MM');
-    let day = newDate.format('DD');
-    return year + month + day;
-  };
-
-  const changeDate = async (e: React.MouseEvent) => {
-    setModalStatus('load');
-    let newDate: moment.Moment = moment();
-    if (date > moment('01-03-2020', 'DD-MM-YYYY') && leftArrowRef.current === e.target) {
-      newDate = date.add(-1, 'days');
-      setDate(newDate);
-    } else if (date < moment().add(-1, 'days')) {
-      newDate = date.add(1, 'days');
-      setDate(newDate);
-    }
-    const searchDate: string = getNewDate(newDate);
-    await getData(`?startCreateDt=${searchDate}&endCreateDt=${searchDate}`);
-    setModalStatus('finished');
-  };
-
-  useEffect(() => {
-    if (data.length === 0) return;
-    setModalStatus('finished');
-    setDate(moment(data[0][0].createDt));
-  }, [data]);
 
   return (
     <>
-      <div id="header">
-        <img className="header-image" src={flag}></img>
-        <div className="header-description">한국 코로나 상황판</div>
-        <div className="header-dummy"></div>
-      </div>
+      <UpperHeader />
       <HeaderButtons />
-      <div className="header-date">
-        <img className="header-date-arrow" src={leftArrow} onClick={changeDate} ref={leftArrowRef}></img>
-        <div className="header-date-text">{`${date.format('YYYY')}년 ${date.format('MM')}월 ${date.format('DD')}일 0시 기준`}</div>
-        <img className="header-date-arrow" src={rightArrow} onClick={changeDate}></img>
-      </div>
+      <HeaderDateInfo data={data} setModalStatus={setModalStatus} getData={getData} />
       <MessagesModal data={data} modalStatus={modalStatus} getData={getData} />
     </>
   );
---
+++ src/components/header/HeaderDateInfo.tsx
@@ -0,0 +1,54 @@
+import { useState, useRef, useEffect, SetStateAction } from 'react';
+import moment from 'moment';
+import { objectType } from '../../App';
+import leftArrow from '../../images/leftArrow.svg';
+import rightArrow from '../../images/rightArrow.svg';
+
+interface headerDateInfoType {
+  data: objectType[][];
+  setModalStatus: React.Dispatch<SetStateAction<string>>;
+  getData: (query: string) => Promise<void>;
+}
+
+const HeaderDateInfo = ({ data, setModalStatus, getData }: headerDateInfoType) => {
+  const [date, setDate] = useState<moment.Moment>(moment());
+  const leftArrowRef = useRef<HTMLImageElement>(null);
+
+  const getNewDate = (newDate: moment.Moment) => {
+    let year = newDate.format('YYYY');
+    let month = newDate.format('MM');
+    let day = newDate.format('DD');
+    return year + month + day;
+  };
+
+  const changeDate = async (e: React.MouseEvent) => {
+    setModalStatus('load');
+    let newDate: moment.Moment = moment();
+    if (date > moment('01-03-2020', 'DD-MM-YYYY') && leftArrowRef.current === e.target) {
+      newDate = date.add(-1, 'days');
+      setDate(newDate);
+    } else if (date < moment().add(-1, 'days')) {
+      newDate = date.add(1, 'days');
+      setDate(newDate);
+    }
+    const searchDate: string = getNewDate(newDate);
+    await getData(`?startCreateDt=${searchDate}&endCreateDt=${searchDate}`);
+    setModalStatus('finished');
+  };
+
+  useEffect(() => {
+    if (data.length === 0) return;
+    setModalStatus('finished');
+    setDate(moment(data[0][0].createDt));
+  }, [data]);
+
+  return (
+    <div className="header-date">
+      <img className="header-date-arrow" src={leftArrow} onClick={changeDate} ref={leftArrowRef}></img>
+      <div className="header-date-text">{`${date.format('YYYY')}년 ${date.format('MM')}월 ${date.format('DD')}일 0시 기준`}</div>
+      <img className="header-date-arrow" src={rightArrow} onClick={changeDate}></img>
+    </div>
+  );
+};
+
+export default HeaderDateInfo;
