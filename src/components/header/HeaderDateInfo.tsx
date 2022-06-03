507b32517a4f58a9690026f3290eef5a5d7082fb
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-06-03 20:37:39 +0900

    [feature] 날짜 이동 modal 기능 추가
    modal background 회색 설정
    modal 바깥부분 클릭 시 모달 제거

--- src/components/header/HeaderDateInfo.tsx
+++ src/components/header/HeaderDateInfo.tsx
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect, Dispatch, SetStateAction } from 'react';
 import moment from 'moment';
 import { objectType } from '../../App';
 import { MODAL_MESSAGES, FETCH_STATUS } from '../../util/constants';
+import HeaderModal from './HeaderModal';
 import leftArrow from '../../images/leftArrow.svg';
 import rightArrow from '../../images/rightArrow.svg';
 
@@ -90,34 +91,14 @@ const HeaderDateInfo = ({ data, modalStatus, setModalStatus, getData }: headerDa
 
   const setModal = () => {
     const modal: JSX.Element = (
-      <div className="header-date-move">
-        <div className="header-date-modal">
-          <div className="header-date-modal-content">
-            이동하고자 하는 날짜를 입력해주세요.<br></br>범위: 20200120 이후 날짜 입력 가능<br></br>(예시: 20220306)
-            <input className="header-date-modal-text" type="text" ref={dateRef} onInput={isInputValid}></input>
-            <div className="header-date-modal-warning">{modalMessage}</div>
-            <div className="header-date-modal-submit-buttons">
-              <div className="header-date-modal-submit" onClick={getDatesData}>
-                이동
-              </div>
-              <div className="header-date-modal-submit" onClick={removeModal}>
-                취소
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <HeaderModal dateRef={dateRef} isInputValid={isInputValid} modalMessage={modalMessage} getDatesData={getDatesData} removeModal={removeModal} />
     );
     setDateModal(modal);
     setModalStatus(FETCH_STATUS.UPDATING);
   };
 
   const moveDate = () => {
-    if (!dateModal.props.children) {
-      setModal();
-    } else {
-      removeModal();
-    }
+    !dateModal.props.children ? setModal() : removeModal();
   };
 
   const getYesterday = () => {
---
+++ src/components/header/HeaderModal.tsx
@@ -0,0 +1,40 @@
+import React, { LegacyRef } from 'react';
+
+interface propsType {
+  dateRef: LegacyRef<HTMLInputElement>;
+  isInputValid: () => boolean;
+  modalMessage: string;
+  getDatesData: () => Promise<void>;
+  removeModal: () => void;
+}
+
+const HeaderModal = ({ dateRef, isInputValid, modalMessage, getDatesData, removeModal }: propsType) => {
+  const handleClickWrapper = (e: React.MouseEvent) => {
+    const WRAPPER_CLASSNAME = 'header-date-move';
+    if (WRAPPER_CLASSNAME === (e.target as HTMLElement).className) {
+      removeModal();
+    }
+  };
+
+  return (
+    <div className="header-date-move" onClick={handleClickWrapper}>
+      <div className="header-date-modal">
+        <div className="header-date-modal-content">
+          이동하고자 하는 날짜를 입력해주세요.<br></br>범위: 20200120 이후 날짜 입력 가능<br></br>(예시: 20220306)
+          <input className="header-date-modal-text" type="text" ref={dateRef} onInput={isInputValid}></input>
+          <div className="header-date-modal-warning">{modalMessage}</div>
+          <div className="header-date-modal-submit-buttons">
+            <div className="header-date-modal-submit" onClick={getDatesData}>
+              이동
+            </div>
+            <div className="header-date-modal-submit" onClick={removeModal}>
+              취소
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderModal;
